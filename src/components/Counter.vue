9f83e21ccc77b7dca63fa341eb3e9ce760b958a4
smbat144 <smbatpaloyan@gmail.com>
2019-04-10 12:56:09 +0400

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Child component</h1>
+        <h1>Counter - {{count}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Counter",
+       props:{
+            count:Number,
+       }
+    }
+</script>
+
+<style scoped>
+div{
+    border: 1px solid red;
+}
+</style>
