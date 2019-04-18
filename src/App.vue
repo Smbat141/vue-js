d134ed3bd78c0ce26cac5cd3cde5599fe6a060cf
smbat144 <smbatpaloyan@gmail.com>
2019-04-18 15:20:49 +0400

    start ToDo

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
     <div class="container">
-        <data-table :items="test"></data-table>
+        
     </div>
 </template>
 
 <script>
-    import DataTable from "./components/DataTable";
-
     export default {
         name: 'app',
         components: {
-            dataTable:DataTable
         },
     }
 </script>
